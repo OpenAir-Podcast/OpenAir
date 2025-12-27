d580d287c336ccfe551c61cd842345779edf6e52
InroEn <developerjennings@gmail.com>
2025-12-27 16:04:45 -0500

    Fix content in the about screen from overflowing int he about page on mobile layout

--- lib/views/settings_pages/about_page.dart
+++ lib/views/settings_pages/about_page.dart
@@ -29,97 +29,101 @@ class AboutPageState extends ConsumerState<AboutPage> {
       appBar: AppBar(
         title: Text(Translations.of(context).text('about')),
       ),
-      body: appInfoAsync.when(
-        data: (data) {
-          return Column(
-            spacing: settingsSpacer,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SizedBox(
-                  height: 150.0,
-                  width: 150.0,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20.0),
-                    child: Image.asset('assets/icons/icon.png'),
+      body: SingleChildScrollView(
+        child: appInfoAsync.when(
+          data: (data) {
+            return Column(
+              spacing: settingsSpacer,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SizedBox(
+                    height: 150.0,
+                    width: 150.0,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Image.asset('assets/icons/icon.png'),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  Translations.of(context).text('aboutDescription'),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
-                    color: Brightness.dark == Theme.of(context).brightness
-                        ? Colors.white
-                        : Colors.black,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    Translations.of(context).text('aboutDescription'),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Brightness.dark == Theme.of(context).brightness
+                          ? Colors.white
+                          : Colors.black,
+                    ),
                   ),
                 ),
-              ),
-              ListTile(
-                leading: Icon(Icons.info_rounded),
-                title: Text(Translations.of(context).text('version')),
-                trailing: Text(
-                  data.version,
+                ListTile(
+                  leading: Icon(Icons.info_rounded),
+                  title: Text(Translations.of(context).text('version')),
+                  trailing: Text(
+                    data.version,
+                  ),
                 ),
-              ),
-              ListTile(
-                leading: Icon(Icons.code_rounded),
-                title: Text(Translations.of(context).text('sourceCode')),
-                onTap: () async {
-                  try {
-                    final String gitHubUrl = dotenv.env['GITHUB_URL']!;
+                ListTile(
+                  leading: Icon(Icons.code_rounded),
+                  title: Text(Translations.of(context).text('sourceCode')),
+                  onTap: () async {
+                    try {
+                      final String gitHubUrl = dotenv.env['GITHUB_URL']!;
 
-                    await launchUrl(Uri.parse(gitHubUrl));
-                  } catch (e) {
-                    if (context.mounted) {
-                      if (!Platform.isAndroid && !Platform.isIOS) {
-                        ref.read(notificationServiceProvider).showNotification(
-                              'OpenAir ${Translations.of(context).text('notification')}',
-                              '${Translations.of(context).text('oopsAnErrorOccurred')} ${Translations.of(context).text('oopsTryAgainLater')}',
-                            );
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(
-                              '${Translations.of(context).text('oopsAnErrorOccurred')} ${Translations.of(context).text('oopsTryAgainLater')}',
+                      await launchUrl(Uri.parse(gitHubUrl));
+                    } catch (e) {
+                      if (context.mounted) {
+                        if (!Platform.isAndroid && !Platform.isIOS) {
+                          ref
+                              .read(notificationServiceProvider)
+                              .showNotification(
+                                'OpenAir ${Translations.of(context).text('notification')}',
+                                '${Translations.of(context).text('oopsAnErrorOccurred')} ${Translations.of(context).text('oopsTryAgainLater')}',
+                              );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(
+                                '${Translations.of(context).text('oopsAnErrorOccurred')} ${Translations.of(context).text('oopsTryAgainLater')}',
+                              ),
                             ),
-                          ),
-                        );
+                          );
+                        }
                       }
                     }
-                  }
-                },
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.gavel_rounded),
+                  title: Text(Translations.of(context).text('licenses')),
+                  onTap: () {
+                    if (Brightness.dark == Theme.of(context).brightness) {
+                      showDarkLicensePage(context);
+                    } else {
+                      showLicensePage(context: context);
+                    }
+                  },
+                ),
+              ],
+            );
+          },
+          error: (error, stackTrace) {
+            return Center(
+                child: Text(
+              Translations.of(context).text('oopsAnErrorOccurred'),
+              style: TextStyle(
+                fontSize: 18.0,
               ),
-              ListTile(
-                leading: Icon(Icons.gavel_rounded),
-                title: Text(Translations.of(context).text('licenses')),
-                onTap: () {
-                  if (Brightness.dark == Theme.of(context).brightness) {
-                    showDarkLicensePage(context);
-                  } else {
-                    showLicensePage(context: context);
-                  }
-                },
-              ),
-            ],
-          );
-        },
-        error: (error, stackTrace) {
-          return Center(
-              child: Text(
-            Translations.of(context).text('oopsAnErrorOccurred'),
-            style: TextStyle(
-              fontSize: 18.0,
-            ),
-          ));
-        },
-        loading: () {
-          return const Center(child: CircularProgressIndicator());
-        },
+            ));
+          },
+          loading: () {
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
     );
   }
