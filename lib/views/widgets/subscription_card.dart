26f627b4c437477cf39a7a0d7014ff8e049a568c
Winroy Jennings <inroEn@proton.me>
2026-04-28 17:42:06 -0500

    refactor: implement subscriptionsWithCountsProvider for optimized subscription data fetching

--- lib/views/widgets/subscription_card.dart
+++ lib/views/widgets/subscription_card.dart
@@ -5,26 +5,8 @@ import 'package:openair/config/config.dart';
 import 'package:openair/hive_models/podcast_model.dart';
 import 'package:openair/hive_models/subscription_model.dart';
 import 'package:openair/providers/audio_provider.dart';
-import 'package:openair/providers/hive_provider.dart';
-import 'package:openair/providers/openair_provider.dart';
-import 'package:openair/services/podcast_index_service.dart';
+import 'package:openair/providers/subscription_providers.dart';
 import 'package:openair/views/main_pages/subscriptions_episodes_page.dart';
-
-final getSubscriptionsCountProvider =
-    FutureProvider.family.autoDispose<String, String>((ref, title) async {
-  HiveService hiveService = ref.watch(openAirProvider).hiveService;
-
-  // Gets episodes count from last stored index of episodes
-  int currentSubEpCount =
-      await hiveService.podcastSubscribedEpisodeCount(title);
-
-  // Gets episodes count from PodcastIndex
-  int podcastEpisodeCount =
-      await ref.read(podcastIndexProvider).getPodcastEpisodeCountByTitle(title);
-
-  int result = podcastEpisodeCount - currentSubEpCount;
-  return '$result';
-});
 
 class SubscriptionCard extends StatelessWidget {
   const SubscriptionCard({
@@ -40,134 +22,150 @@ class SubscriptionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final subCountDataAsyncValue =
-        ref.watch(getSubscriptionsCountProvider(subs[index].title));
+    // Get batched data from the optimized provider
+    final subscriptionsWithCounts = ref.watch(subscriptionsWithCountsProvider);
 
-    return Padding(
-      padding: EdgeInsets.fromLTRB(
-        cardSidePadding,
-        cardTopPadding,
-        cardSidePadding,
-        cardTopPadding,
-      ),
-      child: GestureDetector(
-        onTap: () {
-          ref.read(audioProvider.notifier).currentPodcast =
-              PodcastModel.fromJson(subs[index].toJson());
+    return subscriptionsWithCounts.when(
+      data: (countsMap) {
+        final title = subs[index].title;
+        final countData = countsMap[title] ?? {};
+        final newEpisodes = countData['newEpisodes'] as int? ?? 0;
 
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => SubscriptionsEpisodesPage(
-                podcast: PodcastModel.fromJson(subs[index].toJson()),
-                id: subs[index].id,
-              ),
-            ),
-          );
-        },
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: cardImageShadow,
-                        blurRadius: blurRadius,
-                      )
-                    ],
-                  ),
-                  height: cardImageHeight,
-                  width: cardImageWidth,
-                  child: CachedNetworkImage(
-                    memCacheHeight: cardImageHeight.ceil(),
-                    memCacheWidth: cardImageWidth.ceil(),
-                    imageUrl: subs[index].artwork,
-                    errorWidget: (context, url, error) => Icon(
-                      Icons.error,
-                      size: 56.0,
-                    ),
+        return Padding(
+          padding: EdgeInsets.fromLTRB(
+            cardSidePadding,
+            cardTopPadding,
+            cardSidePadding,
+            cardTopPadding,
+          ),
+          child: GestureDetector(
+            onTap: () {
+              ref.read(audioProvider.notifier).currentPodcast =
+                  PodcastModel.fromJson(subs[index].toJson());
+
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => SubscriptionsEpisodesPage(
+                    podcast: PodcastModel.fromJson(subs[index].toJson()),
+                    id: subs[index].id,
                   ),
                 ),
-                Positioned(
-                  right: 0.0,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: subscriptionCountBoxColor,
+              );
+            },
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: cardImageShadow,
+                            blurRadius: blurRadius,
+                          )
+                        ],
+                      ),
+                      height: cardImageHeight,
+                      width: cardImageWidth,
+                      child: CachedNetworkImage(
+                        memCacheHeight: cardImageHeight.ceil(),
+                        memCacheWidth: cardImageWidth.ceil(),
+                        imageUrl: subs[index].artwork,
+                        errorWidget: (context, url, error) => Icon(
+                          Icons.error,
+                          size: 56.0,
+                        ),
+                      ),
                     ),
-                    height: subscriptionCountBoxSize,
-                    width: subscriptionCountBoxSize,
-                    child: subCountDataAsyncValue.when(
-                      data: (data) {
-                        return Center(
-                          child: Text(
-                            data,
-                            style: TextStyle(
-                              color: subscriptionCountBoxTextColor,
-                              fontSize: subscriptionCountBoxFontSize,
-                              fontWeight: subscriptionCountBoxFontWeight,
-                              overflow: TextOverflow.ellipsis,
+                    if (newEpisodes > 0)
+                      Positioned(
+                        right: 0.0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: subscriptionCountBoxColor,
+                          ),
+                          height: subscriptionCountBoxSize,
+                          width: subscriptionCountBoxSize,
+                          child: Center(
+                            child: Text(
+                              '$newEpisodes',
+                              style: TextStyle(
+                                color: subscriptionCountBoxTextColor,
+                                fontSize: subscriptionCountBoxFontSize,
+                                fontWeight: subscriptionCountBoxFontWeight,
+                                overflow: TextOverflow.ellipsis,
+                              ),
                             ),
                           ),
-                        );
-                      },
-                      error: (error, stackTrace) {
-                        return Center(
-                          child: IconButton(
-                            onPressed: () {
-                              ref.invalidate(subscriptionsProvider);
-                            },
-                            icon: Icon(
-                              Icons.error_outline_rounded,
-                              color: Colors.white,
-                              size: subscriptionCountBoxSize - 12,
-                            ),
-                          ),
-                        );
-                      },
-                      loading: () {
-                        return Center(
-                          child: Text(
-                            '...',
-                            style: TextStyle(
-                              color: subscriptionCountBoxTextColor,
-                              fontSize: subscriptionCountBoxFontSize,
-                              fontWeight: subscriptionCountBoxFontWeight,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        );
-                      },
+                        ),
+                      ),
+                  ],
+                ),
+                Container(
+                  height: cardLabelHeight,
+                  width: cardLabelWidth,
+                  decoration: BoxDecoration(
+                    color: cardLabelBackground,
+                    boxShadow: [
+                      BoxShadow(
+                        color: cardLabelShadow,
+                        blurRadius: blurRadius,
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(cardLabelPadding),
+                    child: Text(
+                      subs[index].title,
+                      maxLines: cardLabelMaxLines,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: cardLabelTextColor,
+                        fontSize: cardLabelFontSize,
+                        fontWeight: cardLabelFontWeight,
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
+          ),
+        );
+      },
+      error: (error, stackTrace) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Icon(Icons.error_outline, color: Colors.grey),
+              const SizedBox(height: 4),
+              Text('Error', style: TextStyle(fontSize: 12)),
+            ],
+          ),
+        );
+      },
+      loading: () => Padding(
+        padding: EdgeInsets.fromLTRB(
+          cardSidePadding,
+          cardTopPadding,
+          cardSidePadding,
+          cardTopPadding,
+        ),
+        child: Column(
+          children: [
             Container(
-              height: cardLabelHeight,
-              width: cardLabelWidth,
+              height: cardImageHeight,
+              width: cardImageWidth,
               decoration: BoxDecoration(
-                color: cardLabelBackground,
-                boxShadow: [
-                  BoxShadow(
-                    color: cardLabelShadow,
-                    blurRadius: blurRadius,
-                  ),
-                ],
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(8),
               ),
-              child: Padding(
-                padding: EdgeInsets.all(cardLabelPadding),
-                child: Text(
-                  subs[index].title,
-                  maxLines: cardLabelMaxLines,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: cardLabelTextColor,
-                    fontSize: cardLabelFontSize,
-                    fontWeight: cardLabelFontWeight,
-                  ),
-                ),
-              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              height: 20,
+              width: cardLabelWidth,
+              color: Colors.grey[300],
             ),
           ],
         ),
