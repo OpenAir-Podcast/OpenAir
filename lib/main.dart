b3414f3eb8d943df1e22ff947bac9ea1225c4b20
Winroy Jennings <inroEn@proton.me>
2026-05-04 16:33:14 -0500

    feat: integrate audio_service for background media playback support on Android

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ void main() async {
       androidNotificationChannelId: 'com.liquidhive.openair',
       androidNotificationChannelName: 'OpenAir Audio',
       androidNotificationOngoing: true,
-      androidStopForegroundOnPause: false,
+      androidStopForegroundOnPause: true,
     ),
   );
 
