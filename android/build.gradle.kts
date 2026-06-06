b859b41f6b7e37e028a8e9d2ff6dbd8c54f657cf
Winroy Jennings <inroEn@proton.me>
2026-06-06 14:48:09 -0500

    fix: update build directory assignment to use direct assignment instead of set() method

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,11 +9,11 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
-rootProject.layout.buildDirectory.set(newBuildDir)
+rootProject.layout.buildDirectory = newBuildDir
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.set(newSubprojectBuildDir)
+    project.layout.buildDirectory = newSubprojectBuildDir
 }
 subprojects {
     project.evaluationDependsOn(":app")
